34072be6d86f2638385a83a79963ece67448eadf
kpriyamani <kpriyamani42005@gmail.com>
2025-09-18 19:57:36 +0530

    Add files via upload

---
+++ simplecalculator.py
@@ -0,0 +1,34 @@
+
+
+# calculator.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print("Select operation: +, -, *, /")
+op = input("Enter operator: ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if op == '+':
+    print("Result:", add(num1, num2))
+elif op == '-':
+    print("Result:", subtract(num1, num2))
+elif op == '*':
+    print("Result:", multiply(num1, num2))
+elif op == '/':
+    print("Result:", divide(num1, num2))
+else:
+    print("Invalid operator")
+
+
+
